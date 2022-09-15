8ac961b7a2689f038b049af1bf522dd0c9a87a90
eliptik <adamfeyk5@gmail.com>
2022-09-15 11:03:15 +0300

    Add playRound function & player-computer selection

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function getComputerChoice() {
     const moves = ["Rock", "Paper", "Scissors"]
     let randomMove = moves[Math.floor(Math.random() * moves.length)];
-    console.log(randomMove);
+    return randomMove;
 }
-getComputerChoice();
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log("player: "+ playerSelection)
+console.log("computer: "+ getComputerChoice())
+
+function playRound(playerSelection, computerSelection){
+}
+
+//console.log(playRound(playerSelection, computerSelection));
+
