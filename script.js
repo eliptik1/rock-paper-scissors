1e6f8a7f3da833889d2d5f0b61bb33b4e9ab0439
eliptik <adamfeyk5@gmail.com>
2022-09-15 21:25:12 +0300

    Create function game() & 5 rounds & score system

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
-function getComputerChoice() {
-    const moves = ["rock", "paper", "scissors"]
-    let randomMove = moves[Math.floor(Math.random() * moves.length)];
-    return randomMove;
+function game(){
+    function getComputerChoice() {
+        const moves = ["rock", "paper", "scissors"]
+        let randomMove = moves[Math.floor(Math.random() * moves.length)];
+        return randomMove;
+    }
+    
+    const playerSelection = prompt();
+    const computerSelection = getComputerChoice();
+    
+    console.log("player: "+ playerSelection.toLowerCase())
+    console.log("computer: "+ computerSelection)
+    
+    function playRound(playerSelection, computerSelection){
+        if(playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"){
+        console.log("You won! Rock beats scissors.")
+        score += 1;
+        } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "paper"){
+         console.log("You lost! Paper beats rock.")
+         
+        } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
+         console.log("Draw")
+         
+    
+        } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "scissors"){
+        console.log("You lost! Scissors beats paper.")
+        
+        } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "paper"){
+         console.log("Draw")
+         
+        } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
+         console.log("You won! Paper beats rock.")
+        score += 1;
+    
+        } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors"){
+        score.log("Draw")
+        
+        } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
+         console.log("You win! Scissors beats paper.")
+        score += 1;
+        } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
+         console.log("You lost! Rock beats scissors.")
+        
+    } 
+    }
+    
+    
+    
+    console.log(playRound(playerSelection, computerSelection));
 }
-
-const playerSelection = "PAPER";
-const computerSelection = getComputerChoice();
-
-console.log("player: "+ playerSelection.toLowerCase())
-console.log("computer: "+ computerSelection)
-
-function playRound(playerSelection, computerSelection){
-    if(playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"){
-    console.log("You won! Rock beats scissors.")
-    return result = "win";
-    } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "paper"){
-     console.log("You lost! Paper beats rock.")
-     return result ="lost";
-    } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
-     console.log("Draw")
-     return result = "draw";
-
-    } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "scissors"){
-    console.log("You lost! Scissors beats paper.")
-    return result ="lost";
-    } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "paper"){
-     console.log("Draw")
-     return result = "draw";
-    } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
-     console.log("You won! Paper beats rock.")
-     return result = "win";
-
-    } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors"){
-    console.log("Draw")
-    return result = "draw";
-    } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
-     console.log("You win! Scissors beats paper.")
-     return result = "win";
-    } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
-     console.log("You lost! Rock beats scissors.")
-     return result ="lost";
-} 
+let score = 0;
+for(let i = 0; i<5; i++){
+    game();
 }
-
-let result;
-
-console.log(playRound(playerSelection, computerSelection));
+if (score >= 3){
+    console.log("Congratulations! You won the game!")
+} else {
+    console.log("You lost :(")
+}
