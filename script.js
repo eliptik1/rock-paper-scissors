1f573dcb8748e7088679a4ce4b1c439fbaec4e73
eliptik <adamfeyk5@gmail.com>
2022-09-15 21:52:21 +0300

    Update score system and output

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function game(){
         return randomMove;
     }
     
-    const playerSelection = prompt();
+    const playerSelection = "rock";
     const computerSelection = getComputerChoice();
     
     console.log("player: "+ playerSelection.toLowerCase())
@@ -15,31 +15,38 @@ function game(){
         if(playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"){
         console.log("You won! Rock beats scissors.")
         score += 1;
+
         } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "paper"){
-         console.log("You lost! Paper beats rock.")
+        console.log("You lost! Paper beats rock.")
+        lose +=1;
          
         } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
-         console.log("Draw")
-         
+        console.log("Draw")
+        draw += 1;
     
         } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "scissors"){
         console.log("You lost! Scissors beats paper.")
+        lose +=1;
         
         } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "paper"){
-         console.log("Draw")
+        console.log("Draw")
+        draw += 1;
          
         } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
-         console.log("You won! Paper beats rock.")
+        console.log("You won! Paper beats rock.")
         score += 1;
-    
+
         } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors"){
         console.log("Draw")
-        
+        draw += 1;
+
         } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
-         console.log("You win! Scissors beats paper.")
+        console.log("You win! Scissors beats paper.")
         score += 1;
+
         } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
-         console.log("You lost! Rock beats scissors.")
+        console.log("You lost! Rock beats scissors.")
+        lose +=1;
         
     } 
     }
@@ -49,11 +56,19 @@ function game(){
     console.log(playRound(playerSelection, computerSelection));
 }
 let score = 0;
+let draw = 0;
+let lose = 0;
 for(let i = 0; i<5; i++){
     game();
 }
-if (score >= 3){
+if (score > lose){
     console.log("Congratulations! You won the game!")
-} else {
-    console.log("You lost :(")
+    console.log("win:", score, " lose:", lose, " draw:", draw)
+} else if(score === lose ) {
+    console.log("The game ended in a draw...")
+    console.log("win:", score, " lose:", lose, " draw:", draw)
+}
+    else {
+    console.log("You lost the game :(")
+    console.log("win:", score, " lose:", lose, " draw:", draw)
 }
