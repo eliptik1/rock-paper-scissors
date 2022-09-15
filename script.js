de02d0d207f4363b57133a251691232dd5851686
eliptik <adamfeyk5@gmail.com>
2022-09-15 12:24:00 +0300

    Add conditions in playRound()

--- script.js
+++ script.js
@@ -4,14 +4,34 @@ function getComputerChoice() {
     return randomMove;
 }
 
-const playerSelection = "rock";
+const playerSelection = "paper";
 const computerSelection = getComputerChoice();
 
 console.log("player: "+ playerSelection)
-console.log("computer: "+ getComputerChoice())
+console.log("computer: "+ computerSelection)
 
 function playRound(playerSelection, computerSelection){
+    if(playerSelection === "rock" && computerSelection === "Scissors"){
+    console.log("You won!rock beats scissors.")
+    } else if(playerSelection === "rock" && computerSelection === "Paper"){
+     console.log("You lost! paper beats rock.")
+    } else if(playerSelection === "rock" && computerSelection === "Rock"){
+     console.log("draw")
+
+    } else if(playerSelection === "paper" && computerSelection === "Scissors"){
+    console.log("You lost!scissors beats paper.")
+    } else if(playerSelection === "paper" && computerSelection === "Paper"){
+     console.log("draw")
+    } else if(playerSelection === "paper" && computerSelection === "Rock"){
+     console.log("You won! paper beats rock.")
+     
+    } else if(playerSelection === "scissors" && computerSelection === "Scissors"){
+    console.log("draw")
+    } else if(playerSelection === "scissors" && computerSelection === "Paper"){
+     console.log("You win! scissors beats paper.")
+    } else if(playerSelection === "scissors" && computerSelection === "Rock"){
+     console.log("You lost! rock beats scissors.")
+} 
 }
 
-//console.log(playRound(playerSelection, computerSelection));
-
+playRound(playerSelection, computerSelection);
