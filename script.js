e1215a0dfd96f823635971f3c9d518adf2befcf1
eliptik <adamfeyk5@gmail.com>
2022-09-15 10:19:27 +0300

    Add random move function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const moves = ["Rock","Paper","Scissors"]
-let randomMove = moves[Math.floor(Math.random() * moves.length)];
-
-function getComputerChoice(){
-   console.log(randomMove);
+function getComputerChoice() {
+    const moves = ["Rock", "Paper", "Scissors"]
+    let randomMove = moves[Math.floor(Math.random() * moves.length)];
+    console.log(randomMove);
 }
 getComputerChoice();
