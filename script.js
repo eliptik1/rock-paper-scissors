e2ba698261bb7e9ec2dbd9f93f17f61f25d3eb81
eliptik <adamfeyk5@gmail.com>
2022-10-08 17:37:55 +0300

    Add move and result outputs & html divs

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+  let score = 0;
+  let draw = 0;
+  let lose = 0;
+  let result = " ";
+
+     const playerScore = document.querySelector("#playerScore")
+     const computerScore = document.querySelector("#computerScore")
+     const gameResult = document.querySelector("#gameResult")
+     const playerMove = document.querySelector("#playerMove")
+     const computerMove = document.querySelector("#computerMove")
+     playerScore.textContent = `Player score: ${score}`
+     computerScore.textContent = `Computer score: ${lose}`
+     gameResult.textContent = `${result}`
+     
+
+    
     const playRock = function(){
         playRound1("rock")
+        playerMove.textContent = "You chose rock"
     }
 
     const playPaper = function(){
         playRound1("paper")
+        playerMove.textContent = "You chose paper"
     }
 
     const playScissors = function(){
         playRound1("scissors")
+        playerMove.textContent = "You chose scissors"
     }
     
     
     function getComputerChoice() {
         const moves = ["rock", "paper", "scissors"]
         let randomMove = moves[Math.floor(Math.random() * moves.length)];
+        computerMove.textContent = `Computer chose ${randomMove}`;
         return randomMove;
     }   
 
@@ -33,8 +53,11 @@
         score = 0;
         draw = 0;
         lose = 0;
-        console.log("score: " + score)
-        console.log("lose: " + lose)
+        playerScore.textContent = `Player score: ${score}`
+        computerScore.textContent = `Computer score: ${lose}`
+        gameResult.textContent = " "
+        playerMove.textContent = " "
+        computerMove.textContent = " "
         rockBtn.addEventListener("click", playRock)
         paperBtn.addEventListener("click", playPaper)
         scissorsBtn.addEventListener("click", playScissors)
@@ -48,83 +71,71 @@
     scissorsBtn.removeEventListener("click", playScissors)
     }
 
-
-    let score = 0;
-    let draw = 0;
-    let lose = 0;
-    
-
-    
     function playRound1(playerSelection){
 
         const computerSelection = getComputerChoice();
 
-        console.log("player: "+ playerSelection.toLowerCase())
-        console.log("computer: "+ computerSelection)
-
         if(playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"){
-        console.log("You won! Rock beats scissors.")
+        result = "You win! Rock beats scissors."
         score += 1;
         updateResults();
         
         } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "paper"){
-        console.log("You lost! Paper beats rock.")
+        result = "You lose! Paper beats rock."
         lose +=1;
         updateResults();
          
         } else if(playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
-        console.log("Draw")
         draw += 1;
+        result = "It's a tie"
         updateResults();
     
         } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "scissors"){
-        console.log("You lost! Scissors beats paper.")
+        result = "You lose! Scissors beats paper."
         lose +=1;
         updateResults();
         
         } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "paper"){
-        console.log("Draw")
         draw += 1;
+        result = "It's a tie"
         updateResults();
          
         } else if(playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
-        console.log("You won! Paper beats rock.")
         score += 1;
+        result = "You win! Paper beats rock." 
         updateResults();
 
         } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors"){
-        console.log("Draw")
         draw += 1;
+        result = "It's a tie"
         updateResults();
 
         } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
-        console.log("You win! Scissors beats paper.")
         score += 1;
+        result = "You win! Scissors beats paper."
         updateResults();
 
         } else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
-        console.log("You lost! Rock beats scissors.")
         lose +=1;
+        result = "You lose! Rock beats scissors."
         updateResults();
     } 
     }
     
     function updateResults(){
-        console.log("score: "+score)
-        console.log("draw: "+draw)
-        console.log("lose: "+lose)
+        playerScore.textContent = `Player score: ${score}`
+        computerScore.textContent = `Computer score: ${lose}`
+        gameResult.textContent = `${result}`
+
+
 
         if (score === 5){
-            
-            console.log("Congratulations! You won the game!")
-            console.log("win:", score, " lose:", lose, " draw:", draw)
+            gameResult.textContent = "Congratulations! You win the game!"
             removeEvent()
         }
 
         if(lose === 5){
-           
-            console.log("You lost the game :(")
-            console.log("win:", score, " lose:", lose, " draw:", draw)
+            gameResult.textContent = "You lost the game :("
             removeEvent()
         }
     };
